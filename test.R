dd43d640c4ea19aba7d18dd944c72c2c2ed390a4
Tomas Granlund <tagglink42@gmail.com>
2020-05-22 11:59:16 +0000

    added plots and graphs for various variables

--- test.R
+++ test.R
@@ -1,4 +1,6 @@
 csv.files = Sys.glob("csv/*.csv")
+# uncomment this when the deprecated log should not be included
+csv.files = c(csv.files, "csv/deprecated/DeprecatedUserLog.csv")
 
 # Global containers
 all.times = c()
@@ -126,4 +128,97 @@ data.means = data.frame(
 
 write.csv(data.means, "means.csv")
 
+custom.boxplot = function(x, y, min, max, title="TITLE", subtitle="subtitle") {
+  boxplot(x[x >= min & x <= max], y[y >= min & y <= max],
+    names=c("Big Joystick", "Small Joystick"),
+    pars=list(
+      boxwex=0.8, staplewex=0.5, outwex=0.5
+    )
+  )
+  title(title, subtitle)
+}
 
+custom.spread = function(x, min, max, width=0.2,
+  title="TITLE", subtitle="subtitle", rug=TRUE) {
+  hist(x[x >= min & x <= max],
+    seq(min, max, width),
+    main=title, sub=subtitle
+  )
+  if (rug == TRUE) {
+    rug(x)
+  }
+}
+
+
+custom.boxplot(
+  all.times.conventional,
+  all.times.kinetics,
+  0.0, 50.0,
+  "Time spent on attacks (s)", "Untrimmed"
+)
+
+custom.boxplot(
+  all.times.conventional,
+  all.times.kinetics,
+  0.0, 20.0,
+  "Time spent on attacks (s)", "0.0 <= t <= 20.0"
+)
+
+custom.boxplot(
+  all.times.conventional,
+  all.times.kinetics,
+  0.0, 6.0, 
+  "Time spent on attacks (s)", "0.0 <= t <= 6.0"
+)
+
+custom.boxplot(
+  all.attempts.conventional,
+  all.attempts.kinetics,
+  0, 10000,
+  "Number of attempts per attack", "Untrimmed"
+)
+
+custom.boxplot(
+  all.attempts.conventional,
+  all.attempts.kinetics,
+  0, 10,
+  "Number of attempts per attack", "0.0 <= x <= 10"
+)
+
+custom.boxplot(
+  all.distance.conventional,
+  all.distance.kinetics,
+  0, 10000,
+  "Finger distance traveled (pixels)", ""
+)
+
+custom.boxplot(
+  all.area.conventional,
+  all.area.kinetics,
+  0, 1000000,
+  "Screen area used (pixels²)", ""
+)
+
+custom.spread(all.times, 0.0, 50.0, 1.0,
+  "Time spent on attacks (s)", "Untrimmed")
+
+custom.spread(all.times, 0.0, 20.0, 0.5,
+  "Time spent on attacks (s)", "0.0 <= t <= 20")
+
+custom.spread(all.times, 0.0, 6.0, 0.2,
+  "Time spent on attacks (s)", "0.0 <= t <= 6.0")
+
+custom.spread(all.attempts, 0.0, 35.0, 1.0,
+  "Attempts per attack", "Untrimmed", FALSE)
+
+custom.spread(all.attempts, 0.0, 10.0, 1.0,
+  "Attempts per attack", "0.0 <= x <= 10.0", FALSE)
+
+custom.spread(all.distance, 0.0, 1500.0, 10.0,
+  "Finger distance traveled (pixels)", "", FALSE)
+
+custom.spread(all.area, 0.0, 1e+05, 500.0,
+  "Screen area used (pixels²)", "", FALSE)
+
+  
+  
